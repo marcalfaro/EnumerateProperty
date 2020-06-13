9328df904c47bb79c0a9ea1cf4ba0f42f7501798
Marc <marcalfaro@live.com>
2020-06-13 23:06:32 +0800

    Add project files.

---
+++ EnumerateProperty/Module1.vb
@@ -0,0 +1,63 @@
+﻿Option Explicit On
+
+Public Class Person
+    Public Property firstName As String
+    Public Property lastName As String
+    Public Property residence As Address
+End Class
+
+Public Class Address
+    Public Property street As String
+    Public Property postalCode As String
+End Class
+
+Module Module1
+
+
+    Sub Main()
+
+        'Instantiate new class
+        Dim student As New Person
+        With student
+            .firstName = "John"
+            .lastName = "Smith"
+            .residence = New Address With {.street = "123 New Street", .postalCode = "9999"}
+        End With
+
+        'Enumerate properties
+        For Each p As System.Reflection.PropertyInfo In student.GetType().GetProperties()
+            If p.CanRead Then
+                Console.WriteLine("{0}: {1}", p.Name, p.GetValue(student, Nothing))  'possible function
+            End If
+        Next
+
+        'You can also drill down nested property values...
+        Console.WriteLine("Student Information:")
+        Console.WriteLine($"Name: {GetPropValue(student, "lastName")} , {GetPropValue(student, "firstName")}")
+        Console.WriteLine($"Street: {GetPropValue(student, "residence.street")} {GetPropValue(student, "residence.postalCode")}")
+
+
+        Console.ReadLine()
+
+    End Sub
+
+    Function GetPropValue(ByVal obj As Object, ByVal propName As String) As Object
+        Dim nameParts As String() = propName.Split("."c)
+        If nameParts.Length = 1 Then Return obj.[GetType]().GetProperty(propName).GetValue(obj, Nothing)
+
+        For Each part As String In nameParts
+
+            If obj Is Nothing Then Return Nothing
+
+            Dim type As Type = obj.[GetType]()
+            Dim info As Reflection.PropertyInfo = type.GetProperty(part)
+
+            If info Is Nothing Then Return Nothing
+
+            obj = info.GetValue(obj, Nothing)
+        Next
+
+        Return obj
+    End Function
+
+End Module
---
+++ EnumerateProperty/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EnumerateProperty")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("EnumerateProperty")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4860c991-af1e-4b5b-bf31-42770b8a7b60")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
